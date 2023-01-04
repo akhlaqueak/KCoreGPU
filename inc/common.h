85c815f6bda42714fdf3326f9d2d2369bf223f0e
akhlaqueak <akhlaque.ak@gmail.com>
2023-01-04 21:36:37 +0300

    Revisions done

--- inc/common.h
+++ inc/common.h
@@ -13,7 +13,7 @@
 #define THID threadIdx.x
 #define WARP_SIZE 32
 #define UINT unsigned int
-#define DS_LOC string("./dataset/")
+#define DS_LOC string("")
 #define OUTPUT_LOC string("../output/")
 #define REP 10
 #define LANEID (THID&31)
